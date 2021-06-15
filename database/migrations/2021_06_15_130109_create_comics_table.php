c0157e7373d5516c821db8aa6d836f9e8784079c
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-06-15 15:37:50 +0200

    first commit

---
+++ database/migrations/2021_06_15_130109_create_comics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 20);
+            $table->string('author', 20);
+            $table->text('description')->nullable();
+            $table->string('image', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
