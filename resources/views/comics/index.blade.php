5b6f20779c6fbc3574dd80fe74a88f9d2ed77647
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-06-15 16:18:03 +0200

    cards stampate con metodo index

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('main_content')
+
+    <section class="container">
+    
+        <h1>Lista dei fumetti:</h1>
+
+        <div class="row">
+
+            @foreach($comics as $comic)
+                <div class=col-3>
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{ $comic->image }}" alt="{{ $comic->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $comic->title }}</h5>
+                            <a href="#" class="btn btn-primary">Maggiori dettagli</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
+        
+        
+    </section>
+
+    
+
+@endsection
